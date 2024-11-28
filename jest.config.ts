54854004cc9f4f6168c14805752d6947482dd38a
AbbasYAli <cycali1@gmail.com>
2024-11-28 11:43:32 +0200

    Update configurations and remove vite.config.ts

--- jest.config.ts
+++ jest.config.ts
@@ -1,9 +1,9 @@
 import type { Config } from 'jest';
 
 const config: Config = {
-    preset: 'ts-jest',
-    testEnvironment: 'node',
-    roots: ['<rootDir>/test'], // Points Jest to your /test directory
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/test'], // Points Jest to your /test directory
 };
 
 export default config;
